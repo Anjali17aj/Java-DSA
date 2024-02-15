a25ff5ac77486253e7e228946f162ae9e707a4a8
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-15 09:27:17 +0530

    Java-Basics

---
+++ 1.Java-Basics/NextLine/NextLine.java
@@ -0,0 +1,10 @@
+public class NextLine {
+    public static void main(String args[]) {
+        System.out.print("My name is Anjali");
+        System.out.print("I am learning Java-DSA");
+        System.out.println("My name is Prachi");
+        System.out.println("I am learning FullStack development");
+        System.out.print("Software engineer\n");
+        System.out.print("Java\nMern stack\n Java Development");
+    }
+}
