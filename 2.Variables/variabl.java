03240ea61182c7f699a770f8222de18eab1ecddc
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-16 09:29:18 +0530

    Variables

---
+++ 2.Variables/variabl.java
@@ -0,0 +1,15 @@
+public class variabl {
+    public static void main(String args[]) {
+        int a = 17;
+        int b = 7;
+        System.out.println("a");
+        System.out.println(a);
+        System.out.println(b);
+        String s = "Anjali";
+        System.out.println(s);
+        a = 99;
+        System.out.println(a);
+        a = b;
+        System.out.println(a);
+    }
+}
