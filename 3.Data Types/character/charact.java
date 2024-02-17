c130c1592d77686c1d9fefa5e59fad44fc491fcc
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-17 09:30:36 +0530

    Data Types

---
+++ 3.Data Types/character/charact.java
@@ -0,0 +1,9 @@
+
+public class charact {
+    public static void main(String args[]) {
+        char ch = 'a';
+        System.out.println(ch);
+        char chi = 'b';
+        System.out.println(chi);
+    }
+}
