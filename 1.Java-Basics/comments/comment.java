14a23bf9c0eccce70a1b29a46a0c2d6653bed910
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-17 10:22:16 +0530

    Java-Basics

---
+++ 1.Java-Basics/comments/comment.java
@@ -0,0 +1,14 @@
+public class comment {
+    public static void main(String args[]) {
+        // assigning a value to ab. It is a single line comment
+
+        /*
+         * Multi line comments
+         * heelo
+         * anjali started revision
+         */
+        int ab = 10;
+        System.out.println(ab);
+    }
+
+}
