dc82f2bfb58cdf16f128893e6a8123b109ab9771
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-17 09:44:58 +0530

    Data Types

--- 3.Data Types/Booleans/bool.java
+++ 3.Data Types/Booleans/bool.java
@@ -2,5 +2,7 @@ public class bool {
     public static void main(String args[]) {
         byte b = 8;
         System.out.println(b);
+        boolean var = false;
+        System.out.println(var);
     }
 }
