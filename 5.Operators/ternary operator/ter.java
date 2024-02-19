55e6b2acc48476a7b9a2c8839e8e793eefeb505b
Anjali17aj <sayhellotoanjali@gmail.com>
2024-02-19 09:40:57 +0530

    Operators

---
+++ 5.Operators/ternary operator/ter.java
@@ -0,0 +1,8 @@
+public class ter {
+    public static void main(String args[]) {
+        int num = 4;
+        String s = ((num % 2) == 0) ? "even" : "odd";
+        System.out.println((s));
+
+    }
+}
